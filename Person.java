c690f36361c1fd4b6768c9b5cf7c15a0d83da6a8
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-11-29 19:35:37 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kiemtra;
 
-/**
- *
- * @author 84363
- */
-public class Person {
+
+public abstract class Person{
+    protected int id;
+   protected String name;
+    protected short age;
+   protected Address address;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, short age, Address address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + '}';
+    }
+    
+   public abstract String getCapacity();
+      
+    
     
 }
