7a9816392b267a7a89c88c124136cec0799a6b5e
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-11-29 19:34:46 +0700

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kiemtra;
 
-/**
- *
- * @author 84363
- */
+
 public class Address {
+    private String district;
+    private String city;
+
+    public Address(String district, String city) {
+        this.district = district;
+        this.city = city;
+    }
+
+    public Address() {
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" + "district=" + district + ", city=" + city + '}';
+    }
+    
+    
     
 }
