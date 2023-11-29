0d7b9329c3c4ed9d627c6b7321c85310993b1784
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-11-29 19:37:10 +0700

    Update StudentServiceImpl.java

--- StudentServiceImpl.java
+++ StudentServiceImpl.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kiemtra;
 
-/**
- *
- * @author 84363
- */
-public class StudentServiceImpl {
+import java.util.*;
+
+
+public class StudentServiceImpl implements StudentService {
+   public static ArrayList<Student> std = new ArrayList<>();
+    @Override
+    public void addStudent(Student t) {
+        std.add(t);
+    }
+
+    @Override
+    public void deleteStudent(int id) {
+        for (Student st: std) {
+            if(st.getId() == id){
+                std.remove(st);
+            }
+        }
+    }
+
+    @Override
+    public void searchStudent(String name) {
+        for (Student st: std) {
+            if(st.getName().contains(name)){
+                System.out.println(st.toString());
+            }
+        }
+    }
+
+    @Override
+    public void sortedStudent() {
+       std.sort((o1, o2) -> {
+            return (int) (o2.getScore() - o1.getScore());
+        });
+    }
+
+    @Override
+    public void showAllStudent() {
+         for (Student st: std) {
+            System.out.println(st.toString());
+        }
+    }
+    
     
 }
+
+  
