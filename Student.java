db444106a6ae0b71a2a6a5161d1310b8c1825562
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-11-29 19:36:12 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kiemtra;
 
-/**
- *
- * @author 84363
- */
-public class Student {
+
+public class Student extends Person{
+    private double score;
+
+    public Student() {
+    }
+
+    public Student(double score) {
+        this.score = score;
+    }
+
+    public Student(double score, int id, String name, short age, Address address) {
+        super(id, name, age, address);
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "score=" + score + '}';
+    }
+
+   
+
+   
+
+    @Override
+    public String getCapacity() {
+       if( score>= 9.0) return "Hoc luc xuat sac" ;
+        if(8<= score && score< 9.0) return "Hoc luc gioi" ;
+         if(6.5<= score && score< 8.0) return "Hoc luc kha" ;
+          if( score < 6.5) return "Hoc luc tb yeu " ;
+          return null;
+    }    
     
 }
+
